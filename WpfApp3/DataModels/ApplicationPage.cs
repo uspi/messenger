3848676ab564ddf195d716595a6fd2b358e373f8
uspi <snowdogloose@gmail.com>
2020-12-16 23:10:36 +0300

    add description

--- WpfApp3/DataModels/ApplicationPage.cs
+++ WpfApp3/DataModels/ApplicationPage.cs
@@ -6,6 +6,9 @@ using System.Threading.Tasks;
 
 namespace WPFClient
 {
+    /// <summary>
+    /// Contains a list of pages available in the application
+    /// </summary>
     public enum ApplicationPage
     {
         Login = 0,
