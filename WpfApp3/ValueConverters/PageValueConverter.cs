a414719732fdb86813a25bedc5a4c0dd4ef222ff
uspi <snowdogloose@gmail.com>
2020-12-16 11:53:20 +0300

    allows you to call the creation of a page from markup using a enum list as a source

---
+++ WpfApp3/ValueConverters/PageValueConverter.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Diagnostics;
+using System.Globalization;
+
+namespace WPFClient
+{
+    /// <summary>
+    /// Converts the <see cref="ApplicationPage"/> to an actual ViewPage
+    /// </summary>
+    class PageValueConverter : ValueConverter<PageValueConverter>
+    {
+        public override object Convert(object value, Type targetType, 
+            object parameter, CultureInfo culture)
+        {
+            // find the page you want
+            switch ((ApplicationPage)value)
+            {
+                case ApplicationPage.Login:
+                    return new LoginPage();
+                default:
+                    Debugger.Break();
+                    return null;
+            }
+        }
+
+        public override object ConvertBack(object value, Type targetType, 
+            object parameter, CultureInfo culture)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
