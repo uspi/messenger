eccfab5dadca249595f262c033c17eba2cad474f
uspi <snowdogloose@gmail.com>
2020-12-16 23:14:47 +0300

    added list of visual themes

---
+++ WpfApp3/DataModels/ApplicationTheme.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WPFClient
+{
+    /// <summary>
+    /// List of visual theme names available in the app
+    /// </summary>
+    public enum ApplicationTheme
+    {
+        Green = 0,
+        Monochrome,
+        Blue
+    }
+}
