8b240afd06e233b351c7d15a78ebcd60c8873452
uspi <snowdogloose@gmail.com>
2020-12-16 11:46:00 +0300

    added several parameters for correct page display, moved events to a separate method

--- WpfApp3/WindowVeiwModel.cs
+++ WpfApp3/WindowVeiwModel.cs
@@ -1,4 +1,4 @@
-﻿using PropertyChanged;
+﻿
 using System;
 using System.Windows.Forms;
 using System.Runtime.InteropServices;
@@ -10,65 +10,88 @@ using System.Diagnostics;
 
 namespace WPFClient
 {
-    [AddINotifyPropertyChangedInterface]
     public class WindowViewModel : Notifier
     {
         internal CurrentOS _CurrentOS { get; } = new CurrentOS();
 
-
         // Window this view model controls
         private Window mWindow;
 
-        // The margin around the window to allow for a drop shadow
-        // размер поля с тень
-        private int mOuterMarginSize = 0;
+        // the margin around the window to allow for a drop shadow
+        private int mOuterMarginSize = 0;//10
 
         // Corner radius of the window
         private int mWindowRadius = 0;
 
+        private int resizeBorder = 6;
+        private int titleHeight = 22;
+
+        private WindowDockPosition mDockPosition { get; set; } = WindowDockPosition.Undocked;
+
         #region Public Properties
 
-        // resize border around the window
-        public int ResizeBorder { get; set; } = 6;
-
-        // The size of the resize border around the window, taking into account the outer margin
-        public Thickness ResizeBorderThickness 
-        { 
-            get => 
-                (mWindow.WindowState == WindowState.Maximized) ? 
-                new Thickness(0) : new Thickness(ResizeBorder); 
-        }
+        public double WindowMinimumWidth { get; set; } = 400;
+        public double WindowMinimumHeight { get; set; } = 450;
 
         // The margin around the window to allow for a drop shadow
         public int OuterMarginSize
         {
-            get => 
-                (mWindow.WindowState == WindowState.Maximized) ? 
-                0 : mOuterMarginSize;
+            get
+            {
+                // If it is maximized or docked, no border
+                return Borderless ? 0 : mOuterMarginSize;
+            }
+
             set => mOuterMarginSize = value;
+        }
+
+        // Corner radius of the window
+        public int WindowRadius
+        {
+            get =>
+                (mWindow.WindowState == WindowState.Maximized) ?
+                0 : mWindowRadius;
+            set => mOuterMarginSize = value;
+        }
+
+        // state true if window expand or docked
+        public bool Borderless 
+        { 
+            get
+            {
+                return (mWindow.WindowState == WindowState.Maximized 
+                    || mDockPosition != WindowDockPosition.Undocked);
+            }
+        }
+
+        // resize border around the window
+        public int ResizeBorder { get => Borderless ? 0 : resizeBorder; }
+
+        // height of the title bar window
+        public int TitleHeight { get => Borderless ? titleHeight : titleHeight - ResizeBorder; }
+
+        public Thickness InnerContentPadding { get; set; } = new Thickness(0);
+
+        // The size of the resize border around the window, taking into account the outer margin
+        public Thickness ResizeBorderThickness
+        {
+            get
+            {
+                return (mWindow.WindowState == WindowState.Maximized) ?
+                    new Thickness(0) : new Thickness(ResizeBorder + OuterMarginSize);
+            }
+
         }
 
         // The margin around the window to allow for a drop shadow
         public Thickness OuterMarginSizeThickness { get => new Thickness(OuterMarginSize); }
 
         // Corner radius of the window
-        public int WindowRadius
-        {
-            get => 
-                (mWindow.WindowState == WindowState.Maximized) ? 
-                0 : mWindowRadius;
-        }
-
-        // Corner radius of the window
         public CornerRadius WindowCornerRadius { get => new CornerRadius(WindowRadius); }
 
-        // height of the title bar window
-        public int TitleHeight { get; set; } = 22;
+        public GridLength TitleHeightGridLength { get => new GridLength(TitleHeight + ResizeBorder); }
 
-        public GridLength TitleHeightGridLength 
-        { 
-            get => new GridLength(TitleHeight);
-        }
+        public ApplicationPage CurrentPage { get; set; } = ApplicationPage.Login;
 
         // actions for system window control buttons
         public ICommand WindowMinimizeCommand { get; set; }
@@ -89,23 +112,29 @@ namespace WPFClient
 
         public WindowViewModel(Window window)
         {
-            float a = _CurrentOS.Version;
-            string systemOSInfo = _CurrentOS.ToString();
 
-            Debug.WriteLine(systemOSInfo);
+            Debug.WriteLine(_CurrentOS.ToString());
             // this.window
             mWindow = window;
 
-            // View model event, listen out for the window resizing
+            // Listen out for the window resizing
             mWindow.StateChanged += (sender, e) =>
             {
-                OnPropertyChanged(nameof(ResizeBorderThickness));
-                OnPropertyChanged(nameof(OuterMarginSize));
-                OnPropertyChanged(nameof(OuterMarginSizeThickness));
-                OnPropertyChanged(nameof(WindowRadius));
-                OnPropertyChanged(nameof(WindowCornerRadius));
-                OnPropertyChanged(nameof(TitleHeightGridLength));
+                // Fire off events for all properties that are affected by a resize
+                WindowResized();
             };
+
+            // View model event, listen out for the window resizing
+            //mWindow.StateChanged += (sender, e) =>
+            //{
+            //    OnPropertyChanged(nameof(Borderless));
+            //    OnPropertyChanged(nameof(ResizeBorderThickness));
+            //    OnPropertyChanged(nameof(OuterMarginSize));
+            //    OnPropertyChanged(nameof(OuterMarginSizeThickness));
+            //    OnPropertyChanged(nameof(WindowRadius));
+            //    OnPropertyChanged(nameof(WindowCornerRadius));
+            //    OnPropertyChanged(nameof(TitleHeightGridLength));
+            //};
 
             // command initialization
             WindowMinimizeCommand = new RelayCommand(() => mWindow.WindowState = WindowState.Minimized);
@@ -115,6 +144,16 @@ namespace WPFClient
             //ShowWindowMenuCommand = new RelayCommand(() => SystemCommands.ShowSystemMenu(mWindow, GetMousePosition()));
 
             var windowResizer = new WindowResizer(mWindow);
+
+            // Listen out for dock changes
+            windowResizer.WindowDockChanged += (dock) =>
+            {
+                // Store last position
+                mDockPosition = dock;
+
+                // Fire off resize events
+                WindowResized();
+            };
         }
 
         #region LoginScreen
@@ -165,6 +204,20 @@ namespace WPFClient
 
             // add the window position so its a "ToScreen"
             return new Point(position.X + mWindow.Left, position.Y + mWindow.Top);
+        }
+
+        // If the window resizes to a special position (docked or maximized)
+        // this will update all required property change events to set the borders and radius values
+        private void WindowResized()
+        {
+            // Fire off events for all properties that are affected by a resize
+            OnPropertyChanged(nameof(Borderless));
+            OnPropertyChanged(nameof(ResizeBorderThickness));
+            OnPropertyChanged(nameof(OuterMarginSize));
+            OnPropertyChanged(nameof(OuterMarginSizeThickness));
+            OnPropertyChanged(nameof(WindowRadius));
+            OnPropertyChanged(nameof(WindowCornerRadius));
+            OnPropertyChanged(nameof(TitleHeightGridLength));
         }
 
         #endregion
