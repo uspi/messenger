9e6a410a377181acb017ffb3b829717062ecc35d
uspi <snowdogloose@gmail.com>
2020-12-17 18:15:58 +0200

    change name "expand button" to "maximaze button"

--- WpfApp3/DataModels/IApplicationThemeProperties.cs
+++ WpfApp3/DataModels/IApplicationThemeProperties.cs
@@ -9,19 +9,18 @@ namespace WPFClient
     public interface IApplicationThemeProperties
     {
         SolidColorBrush HeadLine { get; }
+        SolidColorBrush ContentBody { get; }
 
         #region HeadLine Buttons
         // state non-active
         SolidColorBrush MinimizeButton { get; }
-        SolidColorBrush ExpandButton { get; }
+        SolidColorBrush MaximazeButton { get; }
         SolidColorBrush CloseButton { get; }
 
         // state active
         SolidColorBrush MinimizeButton_Active { get; }
-        SolidColorBrush ExpandButton_Active { get; }
+        SolidColorBrush MaximazeButton_Active { get; }
         SolidColorBrush CloseButton_Active { get; }
-        #endregion
-
-        SolidColorBrush ContentBody { get; }
+        #endregion   
     }
 }
