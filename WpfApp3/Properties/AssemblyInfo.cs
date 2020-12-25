e99bc3ee083b76d633163ce87b1392d74f3bf0a9
uspi <snowdogloose@gmail.com>
2020-12-25 12:29:18 +0200

    added copytight information

--- WpfApp3/Properties/AssemblyInfo.cs
+++ WpfApp3/Properties/AssemblyInfo.cs
@@ -12,7 +12,7 @@ using System.Windows;
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
 [assembly: AssemblyProduct("WpfApp3")]
-[assembly: AssemblyCopyright("Copyright ©  2020")]
+[assembly: AssemblyCopyright("Copyright © 2020 Eduard Chetverkin. All Rights Reserved.")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 
