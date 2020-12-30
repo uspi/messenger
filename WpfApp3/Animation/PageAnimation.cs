d12c7dc16cfcaa0f1df8d4664e527b67cadc249c
uspi <snowdogloose@gmail.com>
2020-12-30 12:23:04 +0200

    list of animations for the page

---
+++ WpfApp3/Animation/PageAnimation.cs
@@ -0,0 +1,17 @@
+﻿namespace WPFClient
+{
+    /// <summary>
+    /// Styles of page animations for appearing/disappearing
+    /// </summary>
+    public enum PageAnimation
+    {
+        // No animation takes place
+        None = 0,
+
+        // The page slides in and fades in, from the right
+        SlideAndFadeInFromRight = 1,
+
+        // The page slides in and fades out to the left 
+        SlideAndFadeOutToLeft = 2
+    }
+}
