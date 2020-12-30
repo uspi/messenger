99bab4f5a16d6f0727d7303fed4ff15b811cac24
uspi <snowdogloose@gmail.com>
2020-12-30 12:26:44 +0200

    common parent for pages

---
+++ WpfApp3/ViewModel/Base/BaseViewModel.cs
@@ -0,0 +1,18 @@
+﻿using System.ComponentModel;
+
+namespace WPFClient
+{
+    /// <summary>
+    /// Base View Model for all in the application. Inherits class <see cref="Notifier"/> 
+    /// which automatically implements interface <see cref="INotifyPropertyChanged"/> 
+    /// for all public properties
+    /// </summary>
+    public class BaseViewModel : Notifier
+    {
+        // version of the windows operating system
+        internal CurrentOS _CurrentOS { get; } = new CurrentOS();
+
+        // start page for the application
+        public ApplicationPage CurrentPage { get; set; } = ApplicationPage.Login;
+    }
+}
