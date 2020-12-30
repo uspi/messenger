430a8d38dd4bf9d9315fee5759702c19ed72fa44
uspi <snowdogloose@gmail.com>
2020-12-30 12:15:47 +0200

    added inheritance for animation page

--- WpfApp3/Pages/LoginPage.xaml.cs
+++ WpfApp3/Pages/LoginPage.xaml.cs
@@ -18,7 +18,7 @@ namespace WPFClient
     /// <summary>
     /// Interaction logic for LoginPage.xaml
     /// </summary>
-    public partial class LoginPage : Page
+    public partial class LoginPage : BasePage<LoginViewModel>
     {
         public LoginPage()
         {
