ece9fdc6145025f711793b86b985c30efd5e499a
uspi <snowdogloose@gmail.com>
2020-12-30 12:29:00 +0200

    allows you to transfer, in addition to the sender, parameters. for example for use in xaml, in passing a command parameter

---
+++ WpfApp3/ViewModel/Base/RelayParameterizedCommand.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Input;
+
+namespace WPFClient
+{
+    class RelayParameterizedCommand : ICommand
+    {
+        //private Action<object> _execute;
+        //private Func<object, bool> _canExecute;
+
+        //public event EventHandler CanExecuteChanged
+        //{
+        //    add => CommandManager.RequerySuggested += value; 
+        //    remove => CommandManager.RequerySuggested -= value; 
+        //}
+
+        //public RelayCommand(Action<object> execute, 
+        //    Func<object, bool> canExecute = null)
+        //{
+        //    _execute = execute;
+        //    _canExecute = canExecute;
+        //}
+
+        //public bool CanExecute(object parameter) => _canExecute == null || _canExecute(parameter);
+
+        //public void Execute(object parameter) => _execute(parameter);
+
+
+        private Action<object> _Action;
+
+        public event EventHandler CanExecuteChanged = (sender, e) => { };
+
+        // constructor
+        public RelayParameterizedCommand(Action<object> action) => _Action = action;
+
+        // A relay command can always execute
+        public bool CanExecute(object parameter) => true;
+
+        public void Execute(object parameter) => _Action(parameter);
+    }
+}
+
