8ad92e11af417544bad0f55c4ff38aeb1af5d6ee
uspi <snowdogloose@gmail.com>
2021-01-03 18:59:56 +0200

    "Run Command" allows you to execute code on the value of the expression flag. Since this is an expression, using it you can pass the property value (for example, from the view model) directly

--- WpfApp3/ViewModel/Base/BaseViewModel.cs
+++ WpfApp3/ViewModel/Base/BaseViewModel.cs
@@ -1,4 +1,7 @@
-﻿using System.ComponentModel;
+﻿using System;
+using System.ComponentModel;
+using System.Linq.Expressions;
+using System.Threading.Tasks;
 
 namespace WPFClient
 {
@@ -13,6 +16,41 @@ namespace WPFClient
         internal CurrentOS _CurrentOS { get; } = new CurrentOS();
 
         // start page for the application
-        public ApplicationPage CurrentPage { get; set; } = ApplicationPage.Login;
+        public ApplicationPage CurrentPage { get; set; } = ApplicationPage.Chat;
+       
+        /// <summary>
+        /// Runs a command if the <paramref name="updatingFlag"/> is not set.
+        /// If the <paramref name="updatingFlag"/> is <see cref="true"/> 
+        /// (indicating the function is already running) then the action is not run.
+        /// If the <paramref name="updatingFlag"/> is <see cref="false"/>
+        /// (indicating no running function) then the action is run
+        /// Once the action is finished if it was run, then the flag is reset to false once done
+        /// </summary>
+        /// <param name="updatingFlag"></param>
+        /// <param name="action">action to run if the command is not already running</param>
+        /// <returns></returns>
+        protected async Task RunCommand(Expression<Func<bool>> updatingFlag, Func<Task> action)
+        // Expression because we want the function to be executed in run time
+        {
+            // check if the flag property is true (function is already running)
+            if (updatingFlag.GetPropertyValue())
+            {
+                return;
+            }
+
+            // set the property flag to true to indicate we are running
+            updatingFlag.SetPropertyValue(true);
+
+            try
+            {
+                // run the passed in action
+                await action();
+            }
+            finally
+            {
+                // now it's finished
+                updatingFlag.SetPropertyValue(false);
+            }
+        }
     }
 }
