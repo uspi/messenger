6d15080b76f3fdb14680faf20a37cf70d230a559
uspi <snowdogloose@gmail.com>
2021-01-03 18:50:00 +0200

    added an interface to bind a property to a view model

--- WpfApp3/Pages/LoginPage.xaml.cs
+++ WpfApp3/Pages/LoginPage.xaml.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Security;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows;
@@ -18,8 +19,11 @@ namespace WPFClient
     /// <summary>
     /// Interaction logic for LoginPage.xaml
     /// </summary>
-    public partial class LoginPage : BasePage<LoginViewModel>
+    public partial class LoginPage : BasePage<LoginViewModel>, IHavePassword
     {
+        // password of this page (LoginPage.xaml)
+        public SecureString SecurePassword { get => PasswordText.SecurePassword; }
+
         public LoginPage()
         {
             InitializeComponent();
