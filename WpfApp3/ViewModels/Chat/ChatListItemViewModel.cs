7157f12d3cf7518166b85feb86e63a29574bf9c1
uspi <snowdogloose@gmail.com>
2021-01-05 10:57:42 +0200

    changed file location

---
+++ WpfApp3/ViewModels/Chat/ChatListItemViewModel.cs
@@ -0,0 +1,29 @@
+﻿namespace WPFClient
+{
+    /// <summary>
+    /// A unit of the chat list, may contain, for example, 
+    /// a dialog with the author's <see cref="Name"/>, 
+    /// his <see cref="ProfileInitials"/> 
+    /// in the picture, the last message and the <see cref="ProfilePictureRGB"/>
+    /// </summary>
+    public class ChatListItemViewModel : BaseViewModel
+    {
+        // indicator of new messages
+        public bool NewMessageAvailable { get; set; }
+
+        // if user click on this item - true
+        public bool IsSelected { get; set; }
+
+        // name displayed in chat list 
+        public string Name { get; set; }
+
+        // latest message displayed in this item
+        public string Message { get; set; }
+
+        // initials default showing in the profile picture
+        public string ProfileInitials { get; set; }
+
+        // profile picture background RGB color in hex
+        public string ProfilePictureRGB { get; set; } 
+    }
+}
---
+++ WpfApp3/ViewModels/Chat/ChatListViewModel.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections.Generic;
+
+namespace WPFClient
+{
+    /// <summary>
+    /// The logic for the chat list. The unit of 
+    /// content is <see cref="ChatListItemViewModel"/>
+    /// </summary>
+    public class ChatListViewModel : BaseViewModel
+    {
+        public List<ChatListItemViewModel> Items { get; set; }
+    }
+}
