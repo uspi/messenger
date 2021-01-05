6814f63eb755a88682a84871ca3bd6f4c5116a86
uspi <snowdogloose@gmail.com>
2021-01-05 12:31:35 +0200

    added files

---
+++ WpfApp3/Pages/SignUpPage.xaml.cs
@@ -0,0 +1,20 @@
+﻿
+using System.Security;
+
+
+namespace WPFClient
+{
+    /// <summary>
+    /// Interaction logic for LoginPage.xaml
+    /// </summary>
+    public partial class SignUpPage : BasePage<LoginViewModel>, IHavePassword
+    {
+        // password of this page (LoginPage.xaml)
+        public SecureString SecurePassword { get => PasswordText.SecurePassword; }
+
+        public SignUpPage()
+        {
+            InitializeComponent();
+        }
+    }
+}
