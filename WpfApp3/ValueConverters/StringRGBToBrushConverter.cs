b2e78c2078418e0abe743694b08cfe1284331ed9
uspi <snowdogloose@gmail.com>
2021-01-05 10:59:25 +0200

    A converter that takes in an RGB string like "f03f0f" and converts it to a wpf "Solid Color Brush"

---
+++ WpfApp3/ValueConverters/StringRGBToBrushConverter.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Globalization;
+using System.Windows.Media;
+
+namespace WPFClient
+{
+    /// <summary>
+    /// A converter that takes in an RGB string like f03f0f and converts it to a wpf <see cref="SolidColorBrush"/>
+    /// </summary>
+    class StringRGBToBrushConverter : ValueConverter<StringRGBToBrushConverter>
+    {
+        public override object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            return (SolidColorBrush)new BrushConverter().ConvertFrom($"#{value}");
+        }
+
+        public override object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
