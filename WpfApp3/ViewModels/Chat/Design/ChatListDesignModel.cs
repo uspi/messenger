1b9b3d0834e0630d0443f73c45e0bfea31f330dd
uspi <snowdogloose@gmail.com>
2021-01-05 11:00:37 +0200

    models with actual data for visual content test will be removed later

---
+++ WpfApp3/ViewModels/Chat/Design/ChatListDesignModel.cs
@@ -0,0 +1,87 @@
+﻿using System.Collections.Generic;
+
+namespace WPFClient
+{
+    // it's for test and debug
+    public class ChatListDesignModel : ChatListViewModel
+    {
+        // instance of this class
+        public static ChatListDesignModel Instance { get => new ChatListDesignModel(); }
+
+        // constructor
+        public ChatListDesignModel() 
+        {
+            base.Items = new List<ChatListItemViewModel>
+            {
+                new ChatListItemViewModel
+                {
+                    ProfileInitials = "SG",
+                    Name = "Saul Goodman",
+                    Message = "Can you do something for me? The best " +
+                        "teachers are those who show you where to look, but don't tell you what to see",
+                    ProfilePictureRGB = "009be3",
+                    NewMessageAvailable = true,
+                    IsSelected = true
+                },
+                new ChatListItemViewModel
+                {
+                    ProfileInitials = "NV",
+                    Name = "Nacho Varga",
+                    Message = "Something to discuss...",
+                    ProfilePictureRGB = "45c439"
+                },
+                new ChatListItemViewModel
+                {
+                    ProfileInitials = "KW",
+                    Name = "Kim Wexler",
+                    Message = "Your documents are ready!",
+                    ProfilePictureRGB = "c41888"
+                },
+                new ChatListItemViewModel
+                {
+                    ProfileInitials = "SG",
+                    Name = "Saul Goodman",
+                    Message = "Can you do something for me? The best " +
+                        "teachers are those who show you where to look, but don't tell you what to see",
+                    ProfilePictureRGB = "009be3"
+                },
+                new ChatListItemViewModel
+                {
+                    ProfileInitials = "NV",
+                    Name = "Nacho Varga",
+                    Message = "Something to discuss...",
+                    ProfilePictureRGB = "45c439"
+                },
+                new ChatListItemViewModel
+                {
+                    ProfileInitials = "KW",
+                    Name = "Kim Wexler",
+                    Message = "Your documents are ready!",
+                    ProfilePictureRGB = "c41888"
+                },
+                new ChatListItemViewModel
+                {
+                    ProfileInitials = "SG",
+                    Name = "Saul Goodman",
+                    Message = "Can you do something for me? The best " +
+                        "teachers are those who show you where to look, but don't tell you what to see",
+                    ProfilePictureRGB = "009be3"
+                },
+                new ChatListItemViewModel
+                {
+                    ProfileInitials = "NV",
+                    Name = "Nacho Varga",
+                    Message = "Something to discuss...",
+                    ProfilePictureRGB = "45c439"
+                },
+                new ChatListItemViewModel
+                {
+                    ProfileInitials = "KW",
+                    Name = "Kim Wexler",
+                    Message = "Your documents are ready!",
+                    ProfilePictureRGB = "c41888"
+                }
+            };
+        }
+    }
+}
---
+++ WpfApp3/ViewModels/Chat/Design/ChatListItemDesignModel.cs
@@ -0,0 +1,20 @@
+﻿
+namespace WPFClient
+{
+    // it's for test and debug
+    public class ChatListItemDesignModel : ChatListItemViewModel
+    {
+        // instance of this class
+        public static ChatListItemDesignModel Instance { get => new ChatListItemDesignModel(); }
+
+        // constructor
+        public ChatListItemDesignModel() 
+        {
+            ProfileInitials = "SG";
+            Name = "Saul Goodman";
+            Message = "Can you do something for me? The best " +
+                "teachers are those who show you where to look, but don't tell you what to see";
+            ProfilePictureRGB = "009be3";
+        }
+    }
+}
