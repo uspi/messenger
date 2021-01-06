f3e84b94b362487e265616ceffce031e8d4cae5d
uspi <snowdogloose@gmail.com>
2021-01-06 14:23:27 +0200

    dependency registration and window display

--- Messenger/App.xaml.cs
+++ Messenger/App.xaml.cs
@@ -1,4 +1,5 @@
-﻿using System.Windows;
+﻿using Messenger.Core;
+using System.Windows;
 
 namespace Messenger
 {
@@ -7,5 +8,18 @@ namespace Messenger
     /// </summary>
     public partial class App : Application
     {
+        // start WPF application method
+        protected override void OnStartup(StartupEventArgs e)
+        {
+            // let standard app startup method
+            base.OnStartup(e);
+
+            // load IoC immediately before anything else
+            IoC.Setup();
+
+            // instead "startup uri" in MainWindow.xaml let's do the same with the code
+            Current.MainWindow = new MainWindow();
+            Current.MainWindow.Show();
+        }
     }
 }
