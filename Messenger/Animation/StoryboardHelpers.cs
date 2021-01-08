76ac0ddd131127850a8d662c478dd6951b5937cb
uspi <snowdogloose@gmail.com>
2021-01-08 17:50:44 +0200

    added different animations and comments for they

--- Messenger/Animation/StoryboardHelpers.cs
+++ Messenger/Animation/StoryboardHelpers.cs
@@ -9,15 +9,22 @@ namespace Messenger
     /// </summary>
     public static class StoryboardHelpers
     {
-        // adds a slide from right animation to the storyboard
+        #region Slide
+
+        /// adds a slide from right animation to the storyboard
+        /// <param name="keepMargin">Whether to keep the element at the same
+        /// width during animation</param>
         public static void AddSlideFromRight(this Storyboard storyboard, float seconds, 
-            double offset, float decelerationRatio = 0.9f)
+            double offset, float decelerationRatio = 0.9f, bool keepMargin = true)
         {
-            // create the margin animate from right
+            // create the animation 
             var animation = new ThicknessAnimation
             {
                 Duration = new Duration(TimeSpan.FromSeconds(seconds)),
-                From = new Thickness(offset, 0, -offset, 0),
+                From = new Thickness(left: keepMargin ? offset : 0, 
+                                     top: 0, 
+                                     right: -offset, 
+                                     bottom: 0),
                 To = new Thickness(0),
                 DecelerationRatio = decelerationRatio
             };
@@ -29,16 +36,21 @@ namespace Messenger
             storyboard.Children.Add(animation);
         }
 
-        // adds a slide to left animation to the storyboard
-        public static void AddSlideToLeft(this Storyboard storyboard, float seconds,
-            double offset, float decelerationRatio = 0.9f)
+        /// adds a slide from left animation to the storyboard
+        /// <param name="keepMargin">Whether to keep the element at the same
+        /// width during animation</param>
+        public static void AddSlideFromLeft(this Storyboard storyboard, float seconds,
+            double offset, float decelerationRatio = 0.9f, bool keepMargin = true)
         {
-            // create the margin animate from right
+            // create the animation 
             var animation = new ThicknessAnimation
             {
                 Duration = new Duration(TimeSpan.FromSeconds(seconds)),
-                From = new Thickness(0),
-                To = new Thickness(-offset, 0, offset, 0),
+                From = new Thickness(left: -offset,
+                                     top: 0,
+                                     right: keepMargin ? offset : 0,
+                                     bottom: 0),
+                To = new Thickness(0),
                 DecelerationRatio = decelerationRatio
             };
 
@@ -49,10 +61,64 @@ namespace Messenger
             storyboard.Children.Add(animation);
         }
 
+        /// adds a slide to left animation to the storyboard
+        /// <param name="keepMargin">Whether to keep the element at the same
+        /// width during animation</param>
+        public static void AddSlideToLeft(this Storyboard storyboard, float seconds,
+            double offset, float decelerationRatio = 0.9f, bool keepMargin = true)
+        {
+            // create the animation 
+            var animation = new ThicknessAnimation
+            {
+                Duration = new Duration(TimeSpan.FromSeconds(seconds)),
+                From = new Thickness(0),
+                To = new Thickness(left: -offset,
+                                   top: 0,
+                                   right: keepMargin ? offset : 0,
+                                   bottom: 0),
+                DecelerationRatio = decelerationRatio
+            };
+
+            // set the target property name
+            Storyboard.SetTargetProperty(animation, new PropertyPath("Margin"));
+
+            // add this to the storyboard
+            storyboard.Children.Add(animation);
+        }
+
+        /// adds a slide to right animation to the storyboard
+        /// <param name="keepMargin">Whether to keep the element at the same
+        /// width during animation</param>
+        public static void AddSlideToRight(this Storyboard storyboard, float seconds,
+            double offset, float decelerationRatio = 0.9f, bool keepMargin = true)
+        {
+            // create the animation
+            var animation = new ThicknessAnimation
+            {
+                Duration = new Duration(TimeSpan.FromSeconds(seconds)),
+                From = new Thickness(0),
+                To = new Thickness(left: keepMargin ? offset : 0,
+                                   top: 0,
+                                   right: -offset,
+                                   bottom: 0),
+                DecelerationRatio = decelerationRatio
+            };
+
+            // set the target property name
+            Storyboard.SetTargetProperty(animation, new PropertyPath("Margin"));
+
+            // add this to the storyboard
+            storyboard.Children.Add(animation);
+        }
+
+        #endregion
+
+        #region Fade
+
         // adds a fade animation to the storyboard
         public static void AddFadeIn(this Storyboard storyboard, float seconds)
         {
-            // create the margin animate from right
+            // create the animation 
             var animation = new DoubleAnimation
             {
                 Duration = new Duration(TimeSpan.FromSeconds(seconds)),
@@ -70,7 +136,7 @@ namespace Messenger
         // adds a fade out animation to the storyboard
         public static void AddFadeOut(this Storyboard storyboard, float seconds)
         {
-            // create the margin animate from right
+            // create the animation 
             var animation = new DoubleAnimation
             {
                 Duration = new Duration(TimeSpan.FromSeconds(seconds)),
@@ -84,5 +150,7 @@ namespace Messenger
             // add this to the storyboard
             storyboard.Children.Add(animation);
         }
+
+        #endregion
     }
 }
