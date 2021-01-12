99fd3202fd007047bde0bc96d121f77d5176d9c3
uspi <snowdogloose@gmail.com>
2021-01-12 09:59:44 +0200

    public method for accessing the current page

--- Messenger.Core/ViewModels/ApplicationViewModel.cs
+++ Messenger.Core/ViewModels/ApplicationViewModel.cs
@@ -1,10 +1,22 @@
-﻿namespace Messenger.Core
+﻿using System;
+
+namespace Messenger.Core
 {
     public class ApplicationViewModel : ViewModelBase
     {
         // start page for the application
-        public ApplicationPage CurrentPage { get; set; } = ApplicationPage.SignIn;
+        public ApplicationPage CurrentPage { get; private set; } = ApplicationPage.SignIn;
 
         public bool SideMenuVisibility { get; set; } = false;
+
+        // navigates to the specified page
+        public void GoToPage(ApplicationPage page)
+        {
+            // set needed page 
+            CurrentPage = page;
+
+            // rule of when to show side menu
+            SideMenuVisibility = page == ApplicationPage.Chat;
+        }
     }
 }
