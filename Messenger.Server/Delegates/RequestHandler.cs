a8929e71538117c1012e838797da8e8a391dbbc7
uspi <snowdogloose@gmail.com>
2021-04-30 11:07:59 +0300

    moved to separate files

---
+++ Messenger.Server/Delegates/RequestHandler.cs
@@ -0,0 +1,7 @@
+﻿using Messenger.Core;
+
+namespace Messenger.Server
+{
+    // delegate for sending request from user enviroment to server
+    public delegate void RequestHandler(UserEnvironment sender, Request request);
+}
---
+++ Messenger.Server/Delegates/ResponseHandler.cs
@@ -0,0 +1,7 @@
+﻿using Messenger.Core;
+
+namespace Messenger.Server
+{
+    // delegate for sending request from server to user enviroment 
+    public delegate void ResponseHandler(UserEnvironment sender, Response request);
+}
