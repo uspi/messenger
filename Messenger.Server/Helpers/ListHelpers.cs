46fa3984127c6e6e6113d959646973e2b7576bc5
uspi <snowdogloose@gmail.com>
2021-04-30 11:13:36 +0300

    extension method for cloning lists

---
+++ Messenger.Server/Helpers/ListHelpers.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace Messenger.Server
+{
+    /// <summary>
+    /// Extensions for working with collections and data
+    /// </summary>
+    static class ListHelpers
+    {
+        // allows you to clone a collection whose elements support the interface ICloneable
+        public static IList<T> Clone<T>(this IList<T> listToClone)
+            where T : ICloneable
+        {
+            return listToClone
+                    .Select(item => (T)item.Clone())
+                    .ToList();
+        }
+    }
+}
